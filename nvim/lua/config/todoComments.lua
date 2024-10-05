47f4263da4aebdab7b0b1a3da2577c5051fd90d7
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-10-05 07:49:17 -0500

    Delete nvim directory

--- nvim/lua/config/todoComments.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.keymap.set
-local todo_comments = require("todo-comments")
-map("n","]t",function()
-	todo_comments.jump_next()
-end, {desc = "Next todo comment"})
-map("n","[t",function()
-	todo_comments.jump_prev()
-end, {desc = "Previous todo comment"})
-todo_comments.setup()
