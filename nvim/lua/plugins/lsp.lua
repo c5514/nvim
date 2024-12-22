8e1cde5afdaa5107e9c84d3c0e8c20baa6808d9b
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-12-22 09:49:38 -0500

    Add files via upload

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,23 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-require("lspconfig").pyright.setup{
-	capabilities = capabilities,
+require("lspconfig").pyright.setup {
+  capabilities = capabilities,
 }
-require("lspconfig").texlab.setup{
-	capabilities = capabilities,
+require("lspconfig").texlab.setup {
+  capabilities = capabilities,
 }
-require("lspconfig").nixd.setup{
-	capabilities = capabilities,
+require("lspconfig").nixd.setup {
+  capabilities = capabilities,
 }
-require("lspconfig").lua_ls.setup{
-	capabilities = capabilities,
+require("lspconfig").lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+    },
+  },
 }
