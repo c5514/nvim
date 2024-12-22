4ee95c346cd6e2300398693587e58c8c7c66759a
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-12-22 09:30:56 -0500

    Add files via upload

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -51,7 +51,8 @@ require("lazy").setup({
       "hrsh7th/cmp-buffer",
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp"
+      "hrsh7th/cmp-nvim-lsp",
+      "f3fora/cmp-spell"
     },
     config = function()
       require "plugins/cmp"
